3a587667fa68cc2a578bd71d8489b66978524508
Vincatis-06 <ch.vslakshmi2015@gmail.com>
2025-05-12 12:08:18 +0530

    committed subgroup master

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GroupMasterController;
-use App\Http\Controllers\ItemMasterController;
-
+use App\Http\Controllers\SubGroupMasterController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
 Route::resource('groupmasters', GroupMasterController::class);
 Route::resource('itemmaster', ItemMasterController::class);
 
+Route::resource('subgroupmaster', SubGroupMasterController::class);
